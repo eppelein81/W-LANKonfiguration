0a70855dbc24f2ad138d5ffa9c3742116ef1e274
Marcus <107219358+eppelein81@users.noreply.github.com>
2024-01-13 23:48:33 +0100

    Update wlan-import.ps1
    Kommentare eingefügt

--- wlan-import.ps1
+++ wlan-import.ps1
@@ -1,6 +1,9 @@
-cls
+#Dateipfad
 $Pfad="C:\Users\$env:username\Desktop\WLAN.xml"
 
-netsh wlan add profile filename=$Pfad user=all
+#das in einer Excel-Tabelle gespeicherte W-LAN Profil wird vom angegebenem Pfad für alle User des PCs gespeichert
+netsh wlan add profile filename="C:\Users\$env:username\Desktop\WLAN.xml" user=all
 pause
+
+#es werden alle vorhanden W-LAN Profile auf dem Rechner angezeigt
 netsh wlan show profiles

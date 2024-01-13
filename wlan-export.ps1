918b07271d2936ab3ed8b548d7d2403c5f95f49f
Marcus <107219358+eppelein81@users.noreply.github.com>
2024-01-13 23:43:51 +0100

    Update wlan-export.ps1
    Kommentare eingefügt

--- wlan-export.ps1
+++ wlan-export.ps1
@@ -1,6 +1,11 @@
-cls
+#Pfad für die Ausgabe wird einer Variablen zugewiesen
 $Pfad="C:\Users\$env:username\Desktop"
 
+#es werden alle vorhanden W-LAN Profile in der Konsole angezeigt
 netsh wlan show profiles
+
+#über eine Eingabemaske kann man nun den Namen des gewünschten W-LANs eintragen welcher einer Variablen zugewiesen wird
 $WLAN = Read-Host "Bitte das gewünschte W-LAN angeben:"
+
+#das W-LAN Profil wird in den angegeben Pfad mit Passwort exportiert
 netsh wlan export profile $WLAN key=clear folder=$Pfad
